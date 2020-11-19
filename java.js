ae070c147cc42cdb713f4e3b35fcf53fafe19104
BerkeNandor <71869010+BerkeNandor@users.noreply.github.com>
2020-11-19 19:44:20 +0100

    java.js letreheozasa

---
+++ java.js
@@ -0,0 +1,12 @@
+function osztalyzas ()
+{
+    var sub1 = parseInt(document.getElementById("1").value);
+    var sub2 = parseInt(document.getElementById("2").value);
+    var sub3 = parseInt(document.getElementById("3").value);
+    var sub4 = parseInt(document.getElementById("4").value);
+    var sub5 = parseInt(document.getElementById("5").value);
+    var sub6 = parseInt(document.getElementById("6").value);
+    var sub7 = parseInt(document.getElementById("7").value);
+    var sub8 = parseInt(document.getElementById("8").value);
+    var sub9 = parseInt(document.getElementById("9").value);
+}
